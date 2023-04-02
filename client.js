e18d7e1d7b8c74f8e7bd8c532c0632b773979956
Friies <bahram.parsa2019@gmail.com>
2023-04-02 15:16:18 -0400

    add client.js & use ES6 shorthand syntax

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+// const { IP, PORT } = require("./constants");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.17.192.24",
+    port: 50541,
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Server returns: ", data);
+  });
+
+  // conn.on("connect", () => {
+  //   console.log("game server connection successful");
+  //   conn.write("Name: AR");
+  // });
+
+
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const net = require("net");
+const  { connect } = require("./client");
+// const  { setupInput } = require("./input");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "172.17.192.24",
-    port: 50541,
-  });
-  
-  conn.on("data", (data) => {
-    console.log("you ded cuz you idled")
-  })
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
